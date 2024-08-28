cac643c13678d256b61a0b7c7fbe95f0ac34ce6a
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-28 12:20:31 +0530

    add a bearer token

--- Spring-JWT-FrontEnd/js/dashboard.js
+++ Spring-JWT-FrontEnd/js/dashboard.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
     let  token = localStorage.getItem("token")
     if(token){
+        $.ajax({
+            url:"http://localhost:8080/api/v1/blog/newMethod",
+            method:"GET",
+            contentType:"application/json",
+            headers:{
+                "Authorization":"Bearer " +token
+            },
+            success:function (response){
+                console.log(response)
 
+            },
+            error:function (error){
+                console.log(error)
+            }
+
+        })
     }else {
         window.location.href="signIn.html"
     }
